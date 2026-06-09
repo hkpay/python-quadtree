2d72d62a34eaa268ec3453c6101f9d947da1b33b
hkpay <hakimpayman@gmail.com>
2026-06-08 21:37:27 -0400

    WIP quadtree implementation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 
+from quadtree import QTreeNode
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
---
+++ quadtree.py
@@ -0,0 +1,9 @@
+class QTreeNode:
+    def __init__(self):
+        self.children = [] # list of QTreeNodes
+        self.level = 0
+
+    def subdivide(self):
+        pass
+
+
