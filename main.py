e65c7d3b4e6b656c9dc46e8644fd4af0371d3207
hkpay <hakimpayman@gmail.com>
2026-06-12 19:00:01 -0400

    quadtree implementation

--- main.py
+++ main.py
@@ -3,27 +3,29 @@ import sys
 import random
 import math
 
-from quadtree import QTreePartition, PointQTreeNode, build_point_qtree
+from quadtree import QTreePartition, PointQTreeNode
 
-SCREEN_MARGIN = 30
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
-SCREEN_SIZE_WITH_MARGINS = (SCREEN_WIDTH - SCREEN_MARGIN, SCREEN_HEIGHT - SCREEN_MARGIN)
 SCREEN_CENTER = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 FPS = 60
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
-POINTS_COUNT = 1000
-POINT_RADIUS = 10
+POINTS_COUNT = 50
+POINT_RADIUS = 1
+POINT_SPAWN_AREA_SIZE = (SCREEN_SIZE[0] - 20, SCREEN_SIZE[1] - 20)
+POINT_SPAWN_AREA_MIN = (10, 10)
+POINT_SPAWN_AREA_MAX = (POINT_SPAWN_AREA_MIN[0] + POINT_SPAWN_AREA_SIZE[0], POINT_SPAWN_AREA_MIN[1] + POINT_SPAWN_AREA_SIZE[1])
 
 def generate_random_points(points_count):
     points = []
 
-    for i in range(points_count):
-        coord = ( random.randint(SCREEN_MARGIN, SCREEN_WIDTH - SCREEN_MARGIN), random.randint(SCREEN_MARGIN, SCREEN_HEIGHT - SCREEN_MARGIN) ) 
+    for _ in range(points_count):
+        coord = ( random.randint(POINT_SPAWN_AREA_MIN[0], POINT_SPAWN_AREA_MAX[0]), random.randint(POINT_SPAWN_AREA_MIN[1], POINT_SPAWN_AREA_MAX[1]) ) 
         keep = True
         for point in points:
             keep = keep and (math.dist(coord, point) > POINT_RADIUS * 2)
@@ -33,9 +35,19 @@ def generate_random_points(points_count):
     return points
 
 def draw_qtree_node(qtreenode, surface):
-    rect = pygame.Rect(0, 0, qtreenode.partition.w * 2, qtreenode.partition.h * 2)
-    rect.center = (qtreenode.partition.x + SCREEN_WIDTH / 2, qtreenode.partition.y + SCREEN_HEIGHT / 2)
-    pygame.draw.rect(surface, WHITE, rect, 1)
+    rect = pygame.Rect(qtreenode.partition.x,qtreenode.partition.y, qtreenode.partition.w, qtreenode.partition.h)
+    pygame.draw.rect(surface, RED, rect, 1)
+
+def draw_qtree(qtreenode, surface):
+    draw_qtree_node(qtreenode, surface)
+    for child in qtreenode.children:
+        draw_qtree(child, surface)
+
+def build_qtree_root(topleftcorner, size, points = []):
+    qtreeroot = PointQTreeNode(QTreePartition(topleftcorner[0], topleftcorner[1], size[0], size[1]))
+    for point in points:
+        qtreeroot.insert(point)
+    return qtreeroot
 
 def main():
     pygame.init()
@@ -45,8 +57,8 @@ def main():
     clock = pygame.time.Clock()
 
     points = generate_random_points(POINTS_COUNT)
-
-    qtree = PointQTreeNode(QTreePartition(0, 0, SCREEN_SIZE_WITH_MARGINS[0] / 2, SCREEN_SIZE_WITH_MARGINS[1] / 2))
+    
+    qtree = build_qtree_root(POINT_SPAWN_AREA_MIN, POINT_SPAWN_AREA_SIZE, points)
 
     running = True
     while running:
@@ -56,12 +68,15 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     points = generate_random_points(POINTS_COUNT)
+                    qtree = build_qtree_root(POINT_SPAWN_AREA_MIN, POINT_SPAWN_AREA_SIZE, points)
                 if event.key == pygame.K_ESCAPE:
                     running = False
         screen.fill(BLACK)
-        draw_qtree_node(qtree, screen)
-        # for point in points:
-        #    pygame.draw.circle(screen, WHITE, point, POINT_RADIUS, 1)
+        myrect = pygame.Rect(POINT_SPAWN_AREA_MIN[0], POINT_SPAWN_AREA_MIN[1], POINT_SPAWN_AREA_SIZE[0], POINT_SPAWN_AREA_SIZE[1])
+        pygame.draw.rect(screen, WHITE, myrect, 1)
+        draw_qtree(qtree, screen)
+        for point in points:
+            pygame.draw.circle(screen, WHITE, point, POINT_RADIUS, 1)
         pygame.display.flip()
         clock.tick(FPS)
 
--- quadtree.py
+++ quadtree.py
@@ -1,4 +1,4 @@
-# A QTreePartition is just a rectangle described by its center and its half extents
+# A QTreePartition is just a rectangle described by its top left corner and its size
 class QTreePartition:
     def __init__(self,x, y, w, h):
         self.x = x;
@@ -11,10 +11,35 @@ class PointQTreeNode:
         self.children = [] # list of QTreeNodes
         self.level = 0
         self.partition = partition
+        self.points = []
+    
+    def contains_point(self, point):
+        x_max = self.partition.x + self.partition.w
+        x_min = self.partition.x
+        y_max = self.partition.y + self.partition.h
+        y_min = self.partition.y
+        contains_x = point[0] < x_max and point[0] >= x_min
+        contains_y = point[1] < y_max and point[1] >= y_min
+        return contains_x and contains_y
+
+    def insert(self, point):
+        if self.contains_point(point):
+            self.points.append(point)
+            if len(self.children) == 0:
+                self.subdivide()
+            for child in self.children:
+                if child.contains_point(point):
+                    child.insert(point)
 
     def subdivide(self):
-        pass
+        north_west_partition = QTreePartition(self.partition.x, self.partition.y, self.partition.w / 2, self.partition.h / 2)
+        self.children.append(PointQTreeNode(north_west_partition))
 
-def build_point_qtree(points):
-    root = PointQTreeNode(QTreePartition(0,0,0,0))
-    return root
+        south_west_partition = QTreePartition(self.partition.x, self.partition.y + self.partition.h / 2, self.partition.w / 2, self.partition.h / 2)
+        self.children.append(PointQTreeNode(south_west_partition))
+
+        south_east_partition = QTreePartition(self.partition.x + self.partition.w / 2, self.partition.y + self.partition.h / 2, self.partition.w / 2, self.partition.h / 2)
+        self.children.append(PointQTreeNode(south_east_partition))
+
+        north_east_partition = QTreePartition(self.partition.x + self.partition.w / 2, self.partition.y, self.partition.w / 2, self.partition.h / 2)
+        self.children.append(PointQTreeNode(north_east_partition))
